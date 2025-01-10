b9594e30223090321fad1b429de1b9f00f236026
Alexander Obando <gerencia@torresoftware.com>
2025-01-10 10:16:05 -0500

    Se agregan los parametros mail_from_address y mail_from_name a la tabla users y a los controladores, aporte del compañero IVAN AVILA

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'id_administrator', 'mail_host', 'mail_port', 'mail_username', 'mail_password', 'mail_encryption'
+        'name', 'email', 'password', 'id_administrator', 'mail_host', 'mail_port', 'mail_username', 'mail_password', 'mail_encryption', 'mail_from_address', 'mail_from_name'
     ];
 
     /**
