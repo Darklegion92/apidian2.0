41045dc0874cc9924decb3be5dd0cdb646c88222
Alexander Obando <gerencia@torresoftware.com>
2025-02-04 10:00:08 -0500

    Se eliminan registros sobrantes de la tabla sub_type_workers

--- app/SubTypeWorker.php
+++ app/SubTypeWorker.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubTypeWorker extends Model
 {
+    protected $table = 'sub_type_workers';
+
     protected $fillable = [
         'name', 'code',
     ];
